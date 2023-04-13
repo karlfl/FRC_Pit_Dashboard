cbdf2e644ceda8be3cce75c0ec4f154fafe05e14
Karl Fleischmann <karlf@live.com>
2023-04-12 21:28:20 -0400

    fixup

--- pitdash/main.py
+++ pitdash/main.py
@@ -4,8 +4,7 @@ from datetime import datetime
 
 from google.appengine.api import memcache, wrap_wsgi_app
 
-from model.frc_api import FrcAPI
-from model.tba_api import TbaAPI
+from model.pitdash import PitData
 
 from flask_caching import Cache
 
@@ -40,27 +39,19 @@ def contact():
     return render_template("contact.html")
 
 @app.route("/pitdash/<season>/<eventCode>/<teamNumber>")
-@cache.memoize(120)
+@cache.memoize(5)
 def pit_dash(season, eventCode, teamNumber):
-    model = FrcAPI(season=season, district_code="FIM", team_number=teamNumber,event_code=eventCode)
-    team_avatar = model.get_avatar_image_encoded()
-    team_details = model.get_team_details()
-    event_details = model.get_event_details()
-    event_ranking = model.get_event_ranking()
-    qual_matches = model.get_event_matches(tournament_level='qual')
-    playoff_matches = model.get_event_matches(tournament_level='playoff')
-    tba_model = TbaAPI(season=season, district_code="FIM", team_number=teamNumber,event_code=eventCode)
-    team_ccwms, team_oprs, team_dprs = tba_model.get_event_team_oprs(eventCode, teamNumber)
-    return render_template("pitdash.html", team = team_details, 
-                           image = team_avatar, 
-                           event_details = event_details, 
-                           event_rank = event_ranking, 
-                           qual_matches = qual_matches,
-                           playoff_matches = playoff_matches,
-                           ccwms=team_ccwms,
-                           oprs=team_oprs,
-                           dprs=team_dprs,
-                           event_code = eventCode.upper())
+    pit_model = PitData(season, teamNumber, eventCode)
+    event = pit_model.get_event_details()
+    team = pit_model.get_team_details()
+    stats = pit_model.get_team_stats()
+    matches = pit_model.get_matches()
+    return render_template("pitdash.html",
+                           season = season, 
+                           event = event, 
+                           team = team,
+                           stats = stats,
+                           matches = matches)
 
 if __name__=='__main__':
     app.run(debug=True)
---
+++ pitdash/model/pitdash.py
@@ -0,0 +1,49 @@
+from model.api.frc_api import FrcAPI
+from model.api.tba_api import TbaAPI
+
+class PitData:
+    def __init__(self, season, team_number, event_code, district_code='FIM') -> None:
+        self.season = season
+        self.district_code = district_code
+        self.team_number = team_number
+        self.event_code = event_code
+        self.frc_model = FrcAPI(season, district_code, team_number,event_code)
+        self.tba_model = TbaAPI(season, district_code, team_number, event_code)
+
+
+    def get_event_details(self):
+        event_details = self.frc_model.get_event_details()
+        return {
+            "code": self.event_code,
+            "name": event_details["name"]
+        }
+    
+    def get_team_details(self):
+        team_avatar = self.frc_model.get_avatar_image_encoded()
+        team_details = self.frc_model.get_team_details()
+        return  {
+            "number": self.team_number,
+            "avatar": team_avatar,
+            "name": team_details["nameShort"]
+        }
+    
+    def get_team_stats(self):
+        event_ranking = self.frc_model.get_event_ranking()
+        team_ccwms, team_oprs, team_dprs = self.tba_model.get_event_team_oprs(self.event_code, self.team_number)
+        return  {
+            "rank": event_ranking["rank"],
+            "wins": event_ranking["wins"],
+            "losses": event_ranking["losses"],
+            "ties": event_ranking["ties"],
+            "ccwms": team_ccwms,
+            "oprs": team_oprs,
+            "dprs": team_dprs,
+        }
+    
+    def get_matches(self):
+        qual_matches = self.frc_model.get_event_matches(tournament_level='qual')
+        playoff_matches = self.frc_model.get_event_matches(tournament_level='playoff')
+        return {
+            "qualification": qual_matches,
+            "playoff": playoff_matches
+        }
